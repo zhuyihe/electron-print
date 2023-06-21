61a667089b43cd7e5ba1d20d2fedde067d2de9d7
zyh <zhuyh@glh.cn>
2023-06-21 16:09:36 +0800

    完成双院区配置

---
+++ src/main/modules/partUpdate/config.js
@@ -0,0 +1,14 @@
+const config = {
+  atmeng: {
+    VUE_APP_UPDATE_URL: "https://10.102.11.76/printsoftware/",
+    VUE_APP_HOST_NAME: "10.102.11.76",
+    VUE_APP_PATH_NAME: "/printsoftware/latest.yml",
+  },
+  cp:{
+    VUE_APP_UPDATE_URL: "https://10.202.6.21/printsoftware/",
+    VUE_APP_HOST_NAME: "10.202.6.21",
+    VUE_APP_PATH_NAME: "/printsoftware/latest.yml",
+  }
+};
+
+module.exports=config
